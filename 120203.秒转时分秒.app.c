2ec6ce7bcde516f22b70e20043ae9199c1977c44
qq1990716 <74294827+qq1990716@users.noreply.github.com>
2020-12-02 16:35:30 +0800

    printf("%d时",b); printf("%d分",c); printf("%d秒",d);

---
+++ 120203.秒转时分秒.app.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c,d,e;
+printf("请输入秒数\n");
+scanf("%d",&a);
+b=a/3600;
+c=(a%3600)/60;           
+d=a-3600*b-60*c;
+
+printf("%d时",b);
+printf("%d分",c);
+printf("%d秒",d);
+}
