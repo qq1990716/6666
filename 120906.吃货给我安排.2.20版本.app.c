4874dd0340f4588400468b33da39adabf17ec64c
qq1990716 <74294827+qq1990716@users.noreply.github.com>
2020-12-10 20:29:00 +0800

    2.20版本

--- 120906.吃货给我安排.2.20版本.app.c
+++ 120906.吃货给我安排.2.20版本.app.c
@@ -4,16 +4,16 @@ void main() {
 	char s1[]="66";
 	printf("吃货的日程总是被安排，请问现在是什么节？\n");	
 	scanf("%s", &s1);
-	if(strcmp(s1,"春节")){
+	if(!strcmp(s1,"春节")){
 	printf("给我安排饺子");
 	}
-	else if(strcmp(s1,"端午节")){
+	else if(!strcmp(s1,"端午节")){
 	printf("给我安排粽子");
 	}
-	else if(strcmp(s1,"中秋节")){
+	else if(!strcmp(s1,"中秋节")){
 	printf("给我安排月饼");
 	}
-	else if(strcmp(s1,"元宵节")){
+	else if(!strcmp(s1,"元宵节")){
 	printf("给我安排元宵");
 	}
 }
