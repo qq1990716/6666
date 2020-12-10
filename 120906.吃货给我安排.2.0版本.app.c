72c35784b60f8cfb31d28c161aaec69389dc4968
qq1990716 <74294827+qq1990716@users.noreply.github.com>
2020-12-10 20:22:12 +0800

    2.0版本

---
+++ 120906.吃货给我安排.2.0版本.app.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<string.h>
+	void main(){
+	char num1[]="春节";
+	char num2[]="端午节";
+	char num3[]="元宵节";
+	char num4[]="中秋节";
+	char num5[]="66666666666666666666666666666666";
+	printf("欢迎来到吃胖小能手，说出你想到的节日，我将推荐东西给你吃\n");
+	scanf("%s",&num5);
+	if(strcmp(num1,num5)==0){
+	printf("饺子");
+	}
+	else if("%s",&num5);
+	if(strcmp(num2,num5)==0){
+	printf("粽子");
+	}
+	else if("%s",&num5);
+	if(strcmp(num3,num5)==0){
+	printf("元宵");
+	}
+	else if("%s",&num5);
+	if(strcmp(num4,num5)==0){
+	printf("月饼");
+	}
+	else printf("乱打什么？吃屎去吧你");
+	}
